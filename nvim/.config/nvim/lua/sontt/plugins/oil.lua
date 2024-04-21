09f605b3d93290cd0b4aa642fa6da8a4e13e5c78
sontruong <thanhson.truong@gmail.com>
2024-04-21 19:26:34 +0700

    unbind <C-p> keymap from `oil`. It conflicts with one use in `Telescope`

--- nvim/.config/nvim/lua/sontt/plugins/oil.lua
+++ nvim/.config/nvim/lua/sontt/plugins/oil.lua
@@ -1,2 +1,8 @@
-require("oil").setup()
+require("oil").setup({
+  keymaps = {
+    ["<C-p>"] = false,
+    ["<leader>pv"] = "actions.preview",
+  },
+})
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "<C-b>", "<CMD>Oil<CR>", { desc = "Open parent directory" })
